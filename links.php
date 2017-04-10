dcef8f85b9ecda96523a4bd3f958c7d47e9fa22d
John Plastow <j2trumpet@gmail.com>
2017-04-10 16:42:02 -0700

    Links update

--- links.php
+++ links.php
@@ -8,7 +8,27 @@
           <h2 class="panel-title">Pagination</h2>
         </div>
         <div class="panel-body">
-          Test
+          <nav aria-label="Page navigation">
+            <ul class="pagination">
+              <li>
+                <a href="#" aria-label="Previous">
+                  <span aria-hidden="true"><i class="mdi mdi-chevron-left"></i></span>
+                </a>
+              </li>
+              <li><a href="#">1</a></li>
+              <li><a href="#">2</a></li>
+              <li><a href="#">3</a></li>
+              <li class="active"><a href="#">4</a></li>
+              <li><a href="#">5</a></li>
+              <li><a href="#">6</a></li>
+              <li><a href="#">7</a></li>
+              <li>
+                <a href="#" aria-label="Next">
+                  <span aria-hidden="true"><i class="mdi mdi-chevron-right"></i></span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
 
@@ -17,7 +37,7 @@
           <h2 class="panel-title">Text Link</h2>
         </div>
         <div class="panel-body">
-          Test
+          <a href="">Text Link Example</a>
         </div>
       </div>
 
